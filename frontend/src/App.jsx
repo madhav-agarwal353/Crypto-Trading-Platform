b0fe4c1620e130b7ac167c102758051bba8eea98
madhav-agarwal353 <madhavagarwal042@gmail.com>
2025-12-08 19:36:19 +0530

    final setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
 import { Button } from './components/ui/button'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className='h-screen bg-black'>
-          <Button>hello world</Button>
-      </div>
-    </>
+    <div className="bg-background text-foreground h-screen">
+      <span class="text-3xl font-bold underline">
+        Hello world!
+      </span>
+      <Button>hello</Button>
+    </div>
   )
 }
 
